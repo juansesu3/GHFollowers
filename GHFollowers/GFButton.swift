a246a6c66e1c44ed9fd6a820eb8d3d707b44072e
juansesu3 <juanseu3@hotmail.com>
2024-02-04 12:48:46 +0100

    FEATURE(components Button and TextField this componets are reutulizable)

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,20 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Juan Suarez on 4/2/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
